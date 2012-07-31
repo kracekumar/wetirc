a68ed10bf6c3a78f2d225fa9608b7c88461016db
kracekumar <kracethekingmaker@gmail.com>
2012-07-31 11:00:06 +0530

    Modified fabfile for gunicorn

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,67 @@
-#! /usr/bin/env python
 from fabric.api import local
+from os import getcwd
+from os.path import sep
 
-def push(branch):
-    local("git push -u origin %s"%branch)
+
+def build_path(path):
+    """
+        Function to build the path, since fabfile.py is placed in
+       current directory, just append the value with current path
+    """
+    if path:
+        return sep.join([getcwd(), path])
+    else:
+        print "path is not mentioned"
 
 
-def pull():
+############################gunicorn configuration##############################
+app_name = 'wetirc'
+workers = 2
+access_log = build_path('access.log')
+error_log = build_path('error.log')
+log_level = 'debug'
+timeout = 120
+port = 27502
+
+start_recv_command = "nohup python2.7 recv.py >> recv.out &"
+start_collector_command = "nohup python2.7 collector.py >> collector.out &"
+
+
+def start_server(pid):
+    command = """gunicorn %s:app --workers=%d --access-logfile %s --error-logfile %s --log-level %s --timeout=%d --daemon --bind=127.0.0.1:%d --pid=%s""" %\
+                 (app_name, workers, access_log, error_log, log_level, timeout, port, build_path("." + pid))
+    print command
+    local(command)
+
+
+def restart_server():
+    try:
+        with open('.pid', 'r') as f:
+            pid = int(f.readline())
+            local("kill -HUP %d" % pid)
+    except IOError:
+        try:
+            with open('..pid', 'r') as f:
+                pid = int(f.readline())
+                local("kill -HUP %d" % pid)
+        except IOError:
+            print "pid file not found"
+    except:
+        print "unable to restart"
+
+
+def push_to_github():
+    local("git push -u origin master")
+
+
+def list_process():
+    local("ps aux | grep %d" % port)
+
+
+def deploy():
     local("git pull")
+    restart_server()
 
 
-def commit(message):
-    local("git commit -am '%s'"%message)
-
-
-def commit_and_push(message, branch):
-    commit(message)
-    push(branch)
-
-
-def track_all():
-    local("git add -A")
-
-
-def track(filename):
-    local("git add %s"%filename)
+def hello():
+    print 'hello, This is test!'
