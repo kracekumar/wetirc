18175e3924bedd2fa461a8f194b43ccd01bfbd27
kracekumar <kracethekingmaker@gmail.com>
2012-07-31 12:33:52 +0530

    removed two files

--- instance/development.py
+++
@@ -1,7 +0,0 @@
-NICK_START = 0
-NICK_END = 670000
-NICKNAME = "wetirc"
-CHANNELS = ["#hasgeek", "#brubeck", "#pypy", "#pocoo", "#crunchbang", "##linux-india"]
-IRCLOG = "/home/kracekumar/Dropbox/codes/python/python/brubeck/sandbox/wetirc/"  #IRCLOGS
-WEBAPP_PORTNO = 12345
-
